3fb38138ace55f3639aad4d0c8a2532e33233f13
Mike Lee <mike.lee816@gmail.com>
2023-04-03 14:56:07 -0400

    Create a new environment with creatures and food. Refresh environment in simulation loop

--- simulation.py
+++ simulation.py
@@ -1,5 +1,7 @@
 import pygame
 from sys import exit
+from Creature import Creature
+from Environment import Environment
 
 pygame.init();
 
@@ -10,11 +12,22 @@ size = width, height = 1200, 800
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 
+creature1 = Creature()
+creature2 = Creature()
+creature3 = Creature()
+creatures = [creature1, creature2, creature3]
+
+environment = Environment(size, 5, 5, creatures)
+environment.spawnFood()
+environment.spawnFood()
+environment.spawnFood()
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    
+
+    environment.refresh(pygame, screen, font)
     pygame.display.update()
     clock.tick(.5)
